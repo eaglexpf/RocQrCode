08603224abebe892eb2a4588ef6c0f4eb6c19ae9
roc.xu.kayun <eaglexpf@gmail.com>
2017-04-11 17:06:24 +0800

    no message

--- RocQrCode.php
+++ RocQrCode.php
@@ -12,6 +12,6 @@ class RocQrCode{
 	 * @param $saveandprint 保存二维码并显示出来；$outfile必须传递图片保存路径
 	 */
 	public static function png($text, $outfile = false, $level = QR_ECLEVEL_L, $size = 3, $margin = 4, $saveandprint=false){
-		return QRcode::png($text, $outfile, $level, $size, $margin ,$saveandprint);
+		return \QRcode::png($text, $outfile, $level, $size, $margin ,$saveandprint);
 	}
 }
